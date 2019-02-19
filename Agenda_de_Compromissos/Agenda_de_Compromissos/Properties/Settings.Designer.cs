157e4abaca33cab786771cf78865e58e98d83c61
Guilherme-SMoraes <guisanmo1@hotmail.com>
2019-02-19 10:48:53 -0300

    Alterações no BD

--- Agenda_de_Compromissos/Agenda_de_Compromissos/Properties/Settings.Designer.cs
+++ Agenda_de_Compromissos/Agenda_de_Compromissos/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Agenda_de_Compromissos.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.3.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,9 +26,9 @@ namespace Agenda_de_Compromissos.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=E:\\Arquivos\\DS\\Agenda_de_Comp" +
-            "romissos\\Agenda_de_Compromissos\\Agenda_de_Compromissos.mdf;Integrated Security=T" +
-            "rue")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\Arquivos\\Aulas-de-desenvol" +
+            "vimento\\Agenda_de_Compromissos\\Agenda_de_Compromissos\\Agenda_de_Compromissos.mdf" +
+            ";Integrated Security=True")]
         public string Agenda_de_CompromissosConnectionString {
             get {
                 return ((string)(this["Agenda_de_CompromissosConnectionString"]));
