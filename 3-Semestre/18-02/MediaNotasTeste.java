e451dc0acd6df22fc84679d61436cdd763935b1d
Guilherme-SMoraes <guisanmo1@hotmail.com>
2019-03-02 12:48:11 -0300

    Finalização da lista de atividades de java

--- 3-Semestre/18-02/MediaNotasTeste.java
+++ 3-Semestre/18-02/MediaNotasTeste.java
@@ -2,32 +2,68 @@ package medianotasteste;
 
 import java.util.Scanner;
 
+
+
+
 public class MediaNotasTeste {
     
-    public static int resultadoMedia(int nota1,int nota2,int nota3){
-        return (nota1+nota2+nota3)/3;
+    public static int resultadoMedia(Aluno a){
+        int media=(a.nota1+a.nota2+a.nota3)/3;
+        System.out.println("("+a.nota1+"+"+a.nota2+"+"+a.nota3+")/3="+media);
+        if(media>7){
+            System.out.println("O aluno "+a.Nome+" foi aprovado");
+        }else{
+            System.out.println("O aluno "+a.Nome+" foi reprovado");
+        }
+        return media;
     }
-    
+     
     public static void main(String[] args) {
         
-        Scanner s=new Scanner(System.in);
-        int nota1,nota2,nota3,media;
+       
         
         // TODO code application logic here
+        processamentoDadosAlunos();
         
-        System.out.println("Digite a 1ª nota");
-        nota1=s.nextInt();
-        System.out.println("Digite a 2ª nota");
-        nota2=s.nextInt();
-        System.out.println("Digite a 3ª nota");
-        nota3=s.nextInt();
-        
-        media=resultadoMedia(nota1,nota2,nota3);
-        
-        System.out.println("("+nota1+"+"+nota2+"+"+nota3+")/3="+media);
         
     }
     
+    public static void processamentoDadosAlunos(){
     
+        Aluno aluno1 = new Aluno("Jo�o");
+        resultadoMedia(aluno1);
+        Aluno aluno2 = new Aluno("Maria");
+        resultadoMedia(aluno2);
+        Aluno aluno3 = new Aluno("Rafael");
+        resultadoMedia(aluno3);
+        Aluno aluno4 = new Aluno("Natalia");
+        resultadoMedia(aluno4);
+        Aluno aluno5 = new Aluno("Marcos");
+        resultadoMedia(aluno5);
+              
     
+    }
+    
+}
+class Aluno{
+    
+    String Nome;
+    int nota1,nota2,nota3,idade;
+   
+    Aluno(String nome){
+        
+        Scanner s=new Scanner(System.in);
+        System.out.println("Digite a 1ª nota");
+        this.nota1=s.nextInt();
+        System.out.println("Digite a 2ª nota");
+        this.nota2=s.nextInt();
+        System.out.println("Digite a 3ª nota");
+        this.nota3=s.nextInt();
+        System.out.println("Digite a idade");
+        this.idade=s.nextInt();
+        
+        this.Nome=nome;
+        
+    }
+
 }
